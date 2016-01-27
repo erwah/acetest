1d2b82e8851509adef35ef7532e450b49499c0b3
erikw <erik.wahlstrom@nexusgroup.com>
2016-01-28 00:13:29 +0100

    Adding mockups for client and resource server

---
+++ client/src/main/java/se/wahlstromstekniska/acetest/client/Client.java
@@ -0,0 +1,42 @@
+package se.wahlstromstekniska.acetest.client;
+
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
+import org.eclipse.californium.core.coap.Response;
+
+import se.wahlstromstekniska.acetest.authorizationserver.Constants;
+import se.wahlstromstekniska.acetest.authorizationserver.DTLSRequest;
+import se.wahlstromstekniska.acetest.authorizationserver.ServerConfiguration;
+import se.wahlstromstekniska.acetest.authorizationserver.resource.TokenRequest;
+import se.wahlstromstekniska.acetest.authorizationserver.resource.TokenResponse;
+
+
+public class Client {
+
+	private static ServerConfiguration config = ServerConfiguration.getInstance();
+	
+	public static void main(String[] args) {
+		TokenRequest req = new TokenRequest();
+		req.setGrantType("client_credentials");
+		req.setAud("tempSensorInLivingRoom");
+		req.setClientID("myclient");
+		req.setClientSecret("qwerty");
+
+		Response response;
+		try {
+			response = DTLSRequest.dtlsRequest("coaps://localhost:"+config.getCoapsPort()+"/"+Constants.TOKEN_RESOURCE, "POST", req.toJson(), MediaTypeRegistry.TEXT_PLAIN);
+
+			TokenResponse tokenResponse = new TokenResponse(response.getPayload());
+			
+			String accessToken = tokenResponse.getAccessToken();
+			
+			System.out.println(accessToken);
+			System.out.println(response);
+			System.out.println("Time elapsed (ms): " + response.getRTT());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+
+	}
+
+}
---
+++ resourceserver/src/main/java/se/wahlstromstekniska/acetest/resourceserver/ResourceServer.java
@@ -0,0 +1,10 @@
+package se.wahlstromstekniska.acetest.resourceserver;
+
+public class ResourceServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
